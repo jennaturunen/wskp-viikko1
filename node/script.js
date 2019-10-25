2b78a932becaeee1350e964e77feace90e3bfd89
jennaturunen <jenna.turunen2@gmail.com>
2019-10-25 22:37:26 +0300

    update

---
+++ node/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const _ = require('lodash');    // tai mikä vaan muu nimi muuttujalle, muista käyttää myös kutsussa
+
+const teksti = _.camelCase('Foo Bar');
+console.log('info', teksti);
+
